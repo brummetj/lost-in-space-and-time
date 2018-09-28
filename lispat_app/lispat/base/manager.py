dd72281308a2846519e7a72586048057c8688e95
ZugNZwang <eric.holguinsanchez@gmail.com>
2018-09-27 22:28:39 -0600

    Fixes

--- lispat_app/lispat/base/manager.py
+++ lispat_app/lispat/base/manager.py
@@ -1,26 +1,28 @@
 import os
 from ..utils.logger import Logger
 from ..factory.document_factory import DocumentFactory
-class CommandManager:
 
+
+class CommandManager:
     """
     CommandManager will be used to handle multiple tasks.
     Such as spin off threads, handle command line inputs,
     and create the necessary communication between sources.
     """
+
     def __init__(self, path):
         try:
             logger = Logger("CommandManager - init")
             full_path = os.path.abspath(path)
             if os.path.isdir(full_path):
-                logger.getLogger().info("CommandManager created with path={}".format(fullpath))
-                self.path = fullpath
+                logger.getLogger().info("CommandManager created with path={}".format(full_path))
+                self.path = full_path
             else:
                 logger.getLogger().error("Directory does not exist")
         except:
             logger.getLogger().error("Error Occured")
+
     def run(self):
-        # Initialize with our docs.
         logger = Logger("CommandManager - init")
         try:
             DocumentFactory(self.path)
