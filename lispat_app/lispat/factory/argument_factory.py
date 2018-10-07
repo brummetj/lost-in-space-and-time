ba535bb5d624723223f5f90d2fdaff3941d38c65
ZugNZwang <eric.holguinsanchez@gmail.com>
2018-10-06 19:43:21 -0600

    Added txt to csv functionality

--- lispat_app/lispat/factory/argument_factory.py
+++ lispat_app/lispat/factory/argument_factory.py
@@ -3,16 +3,14 @@ import csv
 import time
 import docx
 import PyPDF2
-import thread
-import tabula
 import docx2txt
-import textract
 from io import StringIO
 from ..utils.logger import Logger
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 from pdfminer.converter import TextConverter
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+
 
 class ArgumentFactory:
 
@@ -31,48 +29,6 @@ class ArgumentFactory:
         self.csvDir = path + "/csv_Data"
 
     '''
-    Function using PyPDF2 and textract library to extract text from pdfs and
-    store them into an array of text files
-    '''
-    def pypdf_handler(self, data):
-        logger = Logger("ArgumentFactory - init")
-        logger.getLogger().info("ArgumentFactory - PyPDF")
-
-        self.txt = []
-        try:
-            for (file, path) in data:
-                pdf_file = os.path.join(path, file)
-                infile = open(pdf_file, 'rb')
-
-                pdfReader = PyPDF2.PdfFileReader(infile)
-
-                num_pages = pdfReader.numPages
-                count = 0
-                text = ""
-
-                while count < num_pages:
-                    page = pdfReader.getPage(count)
-                    count += 1
-                    text += page.extractText()
-
-                if text != "":
-                    text = text
-                else:
-                    text = textract.process(pdf_file, method='tesseract',
-                                            language='eng')
-
-                file = os.path.splitext(file)[0]
-                textFilename = self.pyPDFdir + "/" + file + ".txt"
-
-                textFile = open(textFilename, "w")
-                textFile.write(text)
-                self.txt.append((textFilename, path))
-        except:
-            logger.getLogger().error("Error Occured")
-
-        return self.txt
-
-    '''
     Function using pdfminer to extract text from pdfs and
     store them into an array of text files
     '''
@@ -80,7 +36,7 @@ class ArgumentFactory:
         logger = Logger("ArgumentFactory")
         logger.getLogger().info("ArgumentFactory - PDFMiner")
 
-        txt = []
+        self.txt = []
         pagenums = set()
         output = StringIO()
         laparams = LAParams()
@@ -109,59 +65,12 @@ class ArgumentFactory:
                         textFile.write(text)
                         logger.getLogger().debug("File - {} in {}"
                                                  .format(file, path))
-                        self.txt.append((textFilename, path))
+                        self.txt.append((textFilename, self.pdfminerDir))
                         infile.close()
 
                     converter.close()
                     output.close
-                except:
-                    logger.getLogger().error("File Failed to Open")
-        except:
-            logger.getLogger().error("Error Occured")
-
-        return self.txt
-
-    '''
-    Function using pdfminer to extract text from pdfs and
-    store them into an array of text files
-    '''
-    def pdfminer_to_csv(self, data):
-        logger = Logger("ArgumentFactory")
-        logger.getLogger().info("ArgumentFactory - PDFMiner")
-
-        txt = []
-        pagenums = set()
-        output = StringIO()
-        laparams = LAParams()
-        manager = PDFResourceManager()
-        converter = TextConverter(manager, output, laparams)
-        interpreter = PDFPageInterpreter(manager, converter)
-
-        try:
-            for (file, path) in data:
-                pdf = os.path.join(path, file)
-
-                logger.getLogger().debug("Opening File: {}".format(pdf))
-                try:
-                    with open(pdf, 'rb') as infile:
-                        logger.getLogger().debug("Opening File Successful")
-
-                        for page in PDFPage.get_pages(infile, pagenums):
-                                interpreter.process_page(page)
-
-                        text = output.getvalue()
-                        textFilename = file + ".txt"
-                        textFile = open(textFilename, "w")
-                        logger.getLogger().debug("File - {} opened for writing"
-                                                 .format(textFilename))
-                        textFile.write(text)
-                        logger.getLogger().debug("File - {} in {}"
-                                                 .format(file, path))
-                        self.txt.append((textFilename, path))
-                        infile.close()
-
-                    converter.close()
-                    output.close
+                    textFile.close()
                 except:
                     logger.getLogger().error("File Failed to Open")
         except:
@@ -227,24 +136,33 @@ class ArgumentFactory:
     Function using tabula library to extract text from word docs and
     store them into an array of csv files
     '''
-    def tabula_handler(self, data):
+    def csv_handler(self):
         logger = Logger("ArgumentFactory")
-        logger.getLogger().info("ArgumentFactory - tabula")
-
-        txtPath = self.pdfminerDir
+        logger.getLogger().info("ArgumentFactory - csv_handler")
 
         try:
-            for (file, txtPath) in data:
-                txtFile = os.path.join(txtPath, file)
+            for file in os.listdir(self.pdfminerDir):
+
+                textFile = self.pdfminerDir + "/" + file
+                print(textFile)
 
                 file = os.path.splitext(file)[0]
                 csvFilename = self.csvDir + "/" + file + ".csv"
 
-                inputFile = open(txtFile, "rb")
+                with open(textFile, 'r', newline='') as inputFile:
+                    print("Text file opened")
+                    reader = csv.reader(inputFile, delimiter=" ")
+                    print("reader created")
+                    with open(csvFilename, 'w', newline='') as outputFile:
+                        print("csv file opened")
+                        writer = csv.writer(outputFile)
+                        print("writer created")
+                        for row in reader:
+                            print("inside for loop")
+                            writer.writerow(row)
 
-                outputFile = csv.writer(open(csvFilename, "wb"))
-
-                outputFile.writerows(inputFile)
+                inputFile.close()
+                outputFile.close()
         except:
             logger.getLogger().error("Error Occured")
 
